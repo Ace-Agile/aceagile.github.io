20f9b3355352d63c030dcd6b2357561dff089c20
Wagner Leonardi <leonardiwagner@gmail.com>
2020-04-08 14:26:32 -0300

    add debugging logs

--- update-lists-size.js
+++ update-lists-size.js
@@ -1,6 +1,11 @@
+console.log("file loaded")
+
 const getCards = (listElement) => listElement.querySelectorAll(".list-cards a")
 
 const updateListsSize = () => {
+  console.log("parent", parent.document.querySelectorAll(".list"))
+  console.log("document", document.querySelectorAll(".list"))
+
   parent.document.querySelectorAll(".list")
     .forEach((list) => {
       const cards = getCards(list)

082c326260ccbf6f6c8e4c8cb263f237e3debfb7
Wagner Leonardi <leonardiwagner@gmail.com>
2020-04-08 14:20:17 -0300

    show list size

---
+++ update-lists-size.js
@@ -0,0 +1,13 @@
+const getCards = (listElement) => listElement.querySelectorAll(".list-cards a")
+
+const updateListsSize = () => {
+  document.querySelectorAll(".list")
+    .forEach((list) => {
+      const cards = getCards(list)
+      const limitBadge = list.querySelector(".js-list-limit-badge")
+      limitBadge.classList.remove("hide")
+      limitBadge.innerHTML = cards.length
+    })
+}
+
+setInterval(() => updateListsSize(), 500)
